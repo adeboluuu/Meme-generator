2fcaa2a677c8f55ee963d5826af021c272823e97
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-10-28 02:02:57 +0100

    Added the data file

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,8 +4,8 @@ function Meme() {
   return (
     <main>
         <form className='form'>
-            <input className='form-input' type='text'/>
-            <input className='form-input' type='text'/>
+            <input className='form-input' type='text' placeholder='Top text'/>
+            <input className='form-input' type='text'placeholder='Lower text'/>
             <button className='form-button'>Get a new meme image 🖼</button>
         </form>
     </main>
