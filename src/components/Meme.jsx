c48e322e3b06e3994dfcce30f8176c89387f3cd7
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-11-02 03:22:42 +0100

    Added the data file

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import memesData from '../../memesData'
 
 function Meme() {
+  function getMemeImage(){
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber]
+  }
   return (
     <main>
-        <form className='form'>
+        <div className='form'>
             <input className='form-input' type='text' placeholder='Top text'/>
             <input className='form-input' type='text'placeholder='Lower text'/>
-            <button className='form-button'>Get a new meme image 🖼</button>
-        </form>
+            <button className='form-button'onClick={getMemeImage}>Get a new meme image 🖼</button>
+        </div>
     </main>
   )
 }
