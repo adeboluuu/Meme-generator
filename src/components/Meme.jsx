f94cb97615a7ae2bc42d19d6635dabe0091ced43
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-11-08 00:07:16 +0100

    made the button functional

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,18 +2,23 @@ import React from 'react'
 import memesData from '../../memesData'
 
 function Meme() {
+  const [memeImage, setMemeImage] = React.useState("")
+   
   function getMemeImage(){
     const memesArray = memesData.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
-    const url = memesArray[randomNumber]
+    setMemeImage(memesArray[randomNumber].url)
+
   }
-  return (
+
+  return ( 
     <main>
         <div className='form'>
             <input className='form-input' type='text' placeholder='Top text'/>
             <input className='form-input' type='text'placeholder='Lower text'/>
             <button className='form-button'onClick={getMemeImage}>Get a new meme image 🖼</button>
         </div>
+        <img src={memeImage} className='meme-image'/>
     </main>
   )
 }
