ea48486a8c37f8b8d903ebbf3f65ae55a3e5fd61
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-11-09 11:19:39 +0100

    styling to the meme-image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -18,7 +18,7 @@ function Meme() {
             <input className='form-input' type='text'placeholder='Lower text'/>
             <button className='form-button'onClick={getMemeImage}>Get a new meme image 🖼</button>
         </div>
-        <img src={memeImage} className='meme-image'/>
+        <img src={memeImage} className=' meme-image'/>
     </main>
   )
 }
