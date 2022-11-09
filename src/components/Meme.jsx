bcbb3d0a737586377db4e56ba43c247fe0722303
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-11-09 12:14:34 +0100

    Changes to the meme function

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 import memesData from '../../memesData'
 
 function Meme() {
-  const [memeImage, setMemeImage] = React.useState("")
-   
+  // const [memeImage, setMemeImage] = React.useState("")
+  const [meme, setMeme] = React.useState(
+    {
+      topText: "",
+      bottomText:"",
+      randomImage: "http://i.imgflip.com/1bij.jpg"
+    }
+  )
+  const [allMemeImages,setAllMemeImages]= React.useState(memesData)
+  
   function getMemeImage(){
     const memesArray = memesData.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
-    setMemeImage(memesArray[randomNumber].url)
+    const url = memesArray[randomNumber].url
+    setMeme(prevMeme => ({
+        ...prevMeme,
+        randomImage: url
+    }))
 
   }
 
@@ -18,7 +30,7 @@ function Meme() {
             <input className='form-input' type='text'placeholder='Lower text'/>
             <button className='form-button'onClick={getMemeImage}>Get a new meme image 🖼</button>
         </div>
-        <img src={memeImage} className=' meme-image'/>
+        <img src={meme.randomImage} className=' meme-image'/>
     </main>
   )
 }
