fa50815d561bde64b42960bc1f38ab698ec5b13a
Adeboluwarin <adeboluwarinthomas@gmail.com>
2022-11-10 20:44:16 +0100

    Changes to the header function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <div className='header'>
         <img src={troll_face}alt='troll-face'className='header-image'/>
         <h2 className='header-title'>Meme Generator</h2>
-        <h4 className='header-project'>React Course - Project 3</h4>
+        <h4 className='header-project'>React Course - Project 3 by Adeboluwarin</h4>
     </div>
   )
 }
